6d3e7f5a15520c9aa06f2ee55fed990f8177bdc6
aCandidMind <opensource@candidminds.com>
2017-07-13 13:51:49 +0200

    Add 2nd argument to catall.sh to be able to pass arbitrary command to find's exec instead of cat

--- catall.sh
+++ catall.sh
@@ -1,8 +1,11 @@
+#!/usr/bin/env bash
+
 pathcol=$(tput bold; tput setaf 4)
 coloff=$(tput sgr0)
 
-root="."               # define path here, not in 'find` arg
-root="${root%/}/"      # add trailing '/'
-name="${1:-*}"
+root="."                # define path here, not in 'find` arg
+root="${root%/}/"       # add trailing '/'
+name="${1:-*}"          # default to '*'
+execCommand="${2:-cat}" # default to 'cat' command
 
-find "$root" -name "$name" -printf "$pathcol------\n\n%p$coloff\n" -exec cat {} \;
+find "$root" -name "$name" -printf "$pathcol------\n\n%p$coloff\n" -exec $execCommand {} \;
