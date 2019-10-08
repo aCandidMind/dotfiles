6e4bb423f267edc6e75f694a3baca568e150a3b5
aCandidMind <opensource@candidminds.com>
2019-10-08 20:24:27 +0200

    Remove whitespace from colorfind.sh

--- colorfind.sh
+++ colorfind.sh
@@ -5,19 +5,19 @@ root="$HOME"       # define path here, not in 'find` arg
 root="${root:-.}"  # default to '.'
 root="${root%/}/"  # add trailing '/'
 #
-find "$root" -maxdepth 1 -name '*' -printf "%y %P\n" | 
+find "$root" -maxdepth 1 -name '*' -printf "%y %P\n" |
   while read -r line ;do
-    case $line in 
-       d   ) printf "%s\n" "$dircol$root$coloff";;  
-       d\ *) printf "%s\n" "$dircol$root${line:2}$coloff";;  
+    case $line in
+       d   ) printf "%s\n" "$dircol$root$coloff";;
+       d\ *) printf "%s\n" "$dircol$root${line:2}$coloff";;
        f\ *) l="$root${line:2}"
              d="${l%/*}/"
              f="${l##*/}"
-             cd -P "$d" 
-             printf "%s" "$dircol$d$coloff"  
+             cd -P "$d"
+             printf "%s" "$dircol$d$coloff"
              ls --color=always -R1 "$f"
              cd - >/dev/null
-             ;; 
-          *) printf "ERROR - type not yet catered for\n";;  
+             ;;
+          *) printf "ERROR - type not yet catered for\n";;
     esac
   done
